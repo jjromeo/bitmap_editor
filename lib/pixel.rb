a0bfe7489253b3d7593a562a57126a95110019c8
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-20 18:16:59 +0100

    Pixel init with a default blank colour

--- lib/pixel.rb
+++ lib/pixel.rb
@@ -1,8 +1,9 @@
 class Pixel
-  attr_reader :row, :column
+  attr_reader :row, :column, :colour
 
   def initialize(row, column)
     @row = row
     @column = column
+    @colour = 'O'
   end
 end
--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Pixel do
     expect(pixel.row).to eq 1
     expect(pixel.column).to eq 5
   end
+
+  it 'starts off blank' do
+    pixel = Pixel.new(0, 0)
+    expect(pixel.colour).to eq 'O'
+  end
 end
