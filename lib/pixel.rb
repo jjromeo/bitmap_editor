e7e8d5c8dcd7c90096ee21d341e40a756d5ef090
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-20 18:17:38 +0100

    Pixel can change its colour

--- lib/pixel.rb
+++ lib/pixel.rb
@@ -1,9 +1,14 @@
 class Pixel
-  attr_reader :row, :column, :colour
+  attr_reader :row, :column
+  attr_accessor :colour
 
   def initialize(row, column)
     @row = row
     @column = column
     @colour = 'O'
+  end
+
+  def paint(colour)
+    self.colour = colour
   end
 end
--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe Pixel do
     pixel = Pixel.new(0, 0)
     expect(pixel.colour).to eq 'O'
   end
+
+  it 'can change its colour' do
+    pixel = Pixel.new(0, 0)
+    expect { pixel.paint('C') }.to change { pixel.colour }.from('O').to('C')
+  end
 end
