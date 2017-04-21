3a41d462c50ae484ea075cbc54a4291b34ee5428
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-21 16:54:11 +0100

    InstructionReader Now handles extra args passed in

--- lib/instruction_reader.rb
+++ lib/instruction_reader.rb
@@ -1,11 +1,11 @@
 class InstructionReader
   COMMANDS = {
-    "I" => :create_image,
-    "C" => :clear,
-    "L" => :paint_pixel,
-    "V" => :paint_vertical_line,
-    "H" => :paint_horizontal_line,
-    'S' => :display_image
+    "I" => [:create_image, 2],
+    "C" => [:clear, 0],
+    "L" => [:paint_pixel, 3],
+    "V" => [:paint_vertical_line, 4],
+    "H" => [:paint_horizontal_line, 4],
+    'S' => [:display_image, 0]
   }
 
   def interpret(string)
@@ -14,15 +14,18 @@ class InstructionReader
     interpret_command(command_letter, args)
   end
 
+  private
+
   def interpret_command(command_letter, args)
-    command = COMMANDS[command_letter] || :invalid_command
-    arguments = sanitize_arguments(args)
+    command, expected_arity = COMMANDS[command_letter] || :invalid_command
+    arguments = sanitize_arguments(args, expected_arity)
     [command, *arguments]
   end
 
-  def sanitize_arguments(args)
-    args.map do |arg|
+  def sanitize_arguments(args, expected_arity)
+    all_args = args.map do |arg|
       arg.to_i == 0 ? arg.to_s : arg.to_i
     end
+    expected_arity.zero? ? [] : all_args.slice(0..(expected_arity - 1))
   end
 end
